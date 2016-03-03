5357a74d7856ef91314a513aad86d3a209ea5bd2
xiefei001 <xiefei001@hotmail.com>
2016-03-03 16:39:50 +0100

    update

--- src/main/java/de/swm/integration/lab1/App.java
+++ src/main/java/de/swm/integration/lab1/App.java
@@ -14,7 +14,7 @@ import org.springframework.messaging.support.MessageBuilder;
 public class App {
 	public static void main(String[] args) throws InterruptedException {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("lab1/context.xml");
-		MessageChannel channel = ctx.getBean("messageChannel", MessageChannel.class);
+		MessageChannel channel = ctx.getBean("messageChannel_2", MessageChannel.class);
 		int index = 0;
 		while(true) {
 			Message<String> message1 = MessageBuilder.withPayload("Hello world -one!" + (index++)).build();
