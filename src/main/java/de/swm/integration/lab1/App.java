8c96c1702594016871d443cde7512668295960ff
xiefei001 <xiefei001@hotmail.com>
2016-03-03 15:52:48 +0100

    Initial commit

---
+++ src/main/java/de/swm/integration/lab1/App.java
@@ -0,0 +1,26 @@
+package de.swm.integration.lab1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
+
+
+
+/**
+ * Hello world!
+ */
+public class App {
+	public static void main(String[] args) throws InterruptedException {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("context.xml");
+		MessageChannel channel = ctx.getBean("messageChannel", MessageChannel.class);
+		int index = 0;
+		while(true) {
+			Message<String> message1 = MessageBuilder.withPayload("Hello world -one!" + (index++)).build();
+			System.out.println("sending message");
+			channel.send(message1);
+			Thread.sleep(1000);
+		}
+	}
+}
