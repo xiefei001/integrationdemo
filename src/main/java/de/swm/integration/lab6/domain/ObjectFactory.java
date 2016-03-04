cc15eba360d0e4816c5cd0b78560d155b4857b5c
xiefei001 <xiefei001@hotmail.com>
2016-03-04 17:27:08 +0100

    add objectfactory

---
+++ src/main/java/de/swm/integration/lab6/domain/ObjectFactory.java
@@ -0,0 +1,40 @@
+package de.swm.integration.lab6.domain;
+
+/**
+ * Created by xie.fei on 04.03.2016.
+ */
+import javax.xml.bind.annotation.XmlRegistry;
+
+
+@XmlRegistry
+public class ObjectFactory {
+
+
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link Shiporder }
+     *
+     */
+    public Shiporder createShiporder() {
+        return new Shiporder();
+    }
+
+    /**
+     * Create an instance of {@link Shiporder.Shipto }
+     *
+     */
+    public Shiporder.Shipto createShiporderShipto() {
+        return new Shiporder.Shipto();
+    }
+
+    /**
+     * Create an instance of {@link Shiporder.Item }
+     *
+     */
+    public Shiporder.Item createShiporderItem() {
+        return new Shiporder.Item();
+    }
+
+}
